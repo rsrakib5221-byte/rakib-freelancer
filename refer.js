be7a1fab528f1ab810fb033c4b22e042679a3a91
rsrakib5221-byte <rsrakib5221@gmail.com>
2026-08-01 11:39:34 +0600

    Update refer.js

--- refer.js
+++ refer.js
@@ -6,8 +6,15 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  // এখানে আপনার Firebase Config বসবে
+  apiKey: "AIzaSyAaQIX6yMxjf6bkHD6HPSImNF3o7r0fZvo",
+  authDomain: "rakib-freelancer.firebaseapp.com",
+  projectId: "rakib-freelancer",
+  storageBucket: "rakib-freelancer.firebasestorage.app",
+  messagingSenderId: "1 ঘী72744042815",
+  appId: "1:172744042815:web:2b77927b0ecacf59d00b8a",
+  measurementId: "G-FKFC3JM9XR"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
